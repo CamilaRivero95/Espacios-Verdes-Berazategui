4c41bb5a367dda5daebe4be9fd620e745334db4e
CamilaRivero95 <148718789+CamilaRivero95@users.noreply.github.com>
2023-10-22 18:12:15 -0300

    Add files via upload

---
+++ index.vb
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" href="css/stilos.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>EVPU de Berazategui</title>
+
+</head>
+<body> 
+    <header>
+    <nav>
+        <a href="" class="indice"><b>Inicio</b></a>
+        <a href="./imagenes/bera-plaza1.jpg" class="indice"><b>Imagenes<b></a>
+        <a href="./imagenes/ima6.jpg" class="indice"><b>Parque Ranelagh</b></a>
+        <a href="./imagenes/ima7.jpg" class="indice"><b>Plaza Tiscornia</b></a>
+    </nav>
+    </header>
+    <div class="encabezado">
+        <h1>Espacios Verdes Publicos de Berazategui</h1>
+
+    </div>
+    <main class="margenTotal">
+        <section>
+            <div class="slider-frame"> 
+                <ul> 
+                    <li><img src="./imagenes/ima1.jpg" alt="Plaza Bera"></li>
+                    <li><img src="./imagenes/ima2.jpg" alt="Plaza Bera2"></li>
+                    <li><img src="./imagenes/ima4.jpg" alt="Plaza Bera3"></li>
+                    <li><img src="./imagenes/ima7.jpg" alt="Plaza Bera4"></li>
+                </ul>
+            </div>
+        </section>
+
+        <section>
+            <h2 class="tituloCentrado">El partido de Berazategui</h2>
+            <br>
+            <div class="columnaText"> 
+                <p class="parrafoArt"> Berazategui se ubica al sudeste de la Región Metropolitana de Buenos Aires, limitando al
+                norte con el partido de Quilmes, al oeste con el partido de Florencio Varela, al sur con los partidos de
+                La Plata y Ensenada y al este con el Rio de La Plata. El mismo forma parte del segundo cordón del
+                Gran Buenos Aires y se comunica con las principales ciudades del sur del conurbano a través del
+                Ferrocarril General Roca y la Autopista Buenos Aires – La Plata.
+                <br/> <br/>El partido se divide en nueve localidades, siendo la localidad Berazategui su cabecera. Dentro de ella se
+                encuentra el Edificio Municipal, y otros espacios culturalmente importantes como el Complejo
+                Municipal El Patio, el Centro Cultural Rigolleau y el Club Ducilo. Las demás localidades que lo
+                componen son Berazategui Oeste, Sourigues, Ranelagh, Villa España, Platanos, Hudson, Juan María
+                Gutiérrez y Centro Agricola “El Pato”, esta ultima ha crecido en los últimos años adquiriendo un
+                importante desarrollo urbanístico.
+                </p>
+                <p class="parrafoArt"> 
+                En esta oportunidad se analizaran los espacios verdes públicos que se encuentran dentro de las zonas
+                urbanas de Berazategui, quedando excluidos aquellos que se encuentran en zonas agropecuarias o de
+                reserva, debido a que el estudio en estas zonas requieren de otro tipo de análisis orientado en una
+                mayor medida a su planificación ante un posible crecimiento demográfico. 
+                <br><br> El partido de Berazategui comprende una superficie total de 221,01 km², la cual se encuentra
+                distribuida en un 9% de zona rural, un 31% de zona urbana, y el resto, fue declarado por la UNESCO
+                    como Reserva de Biósfera de la Humanidad en el año 2008.
+                    De acuerdo con el Censo realizado en el año 2001 contaba con una población de 286.735 habitantes y
+                    una densidad poblacional de 1.297,38 hab/km² . Ya para el año 2010 su población era de 369.294
+                    habitantes, y su densidad poblacional de 1.670,94 hab/km². 
+                </p>
+            </div>
+            <br><br>
+            <hr>
+        </section>
+
+        <section>
+            <br> <br>
+            <h2 class="tituloCentrado">Definicion de Espacio Verde Publico</h2>
+            <br>
+            <div class="columnaText"> 
+                <p class="parrafoArt"> Según las distintas definiciones, estas pueden abarcar o no a lo que se conoce como parques, plazas,
+                plazoletas, espacios verdes pequeños, corredores verdes peatonales y viales, arbolado publico y reserva
+                naturales. Todos estos comparten rasgos comunes como ser espacios abiertos con la presencia de
+                arboles, arbustos y césped (color rosa citar).  <br><br> Debido a estas cualidades estos espacios permiten que los
+                ciudadanos mantengan contacto con la naturaleza, realicen actividades deportivas, de recreación y
+                permiten la relajación y el descanso. <br><br>
+                El municipio de Berazategui a través de la Ordenanza N.º 1347-HCD-86 promulgada por el decreto N°
+                938/00 realiza su propia definición de los espacios verdes. El mismo establece que que se conoce
+                como: 
+                </p>
+                <p class="parrafoArt">
+                <b>Parque Publico:</b> Ámbito de importante confrontación natural cuya superficie supera las cuatro
+                hectáreas y destinado a servir a la recreación de la comunidad y la depuración del ambiente.
+                <br><br><b>Plaza Pública:</b> Ámbito de esparcimiento público, ubicado dentro del área urbanizada, cuya superficie
+                no supera las cuatro hectáreas y con neta función comunitaria hacia los núcleos próximos (barrios)
+                aptas para congregar a la población en actos públicos, actividades culturales y descanso.</li> 
+                <br><br><b> Plazoleta:</b> Pequeño espacio verde ubicado generalmente en la intersección de calles y/o avenidas con
+                árboles o arbustos, destinados al solaz de la población.      
+                </p>
+            </div>
+                <br><br>
+                <hr>
+        </section>
+            <h2 class="tituloCentrado">¿Cuales son sus beneficios?</h2>  
+            <div class="columnaText">
+                <p class="parrafoArt"> Los espacios verdes en las ciudades no solo cumplen la función de embellecerlas.
+                Estos debido a sus propiedades intrínsecas, contribuyen a minimizar los impactos de la rápida urbanización, como la contaminación del aire y agua, la alta densidad de tránsito y
+                transporte, el ruido excesivo y el hacinamiento. 
+            <br><br>
+                Entre los beneficios que son capaces de producir estos espacios podemos mencionar
+                algunos de ellos, tales como: <br><br>
+                Actúan como reguladores del clima evitando que se generen microclimas, compensando los valores de temperatura y de humedad que desequilibran las construcciones de superficies planas como los pavimentos y edificios los cuales son más capaces de absorber las radiaciones solares y emitir posteriormente energía calórica. La
+                presencia de estos espacios y su arbolado es importante para el contraste térmico, es
+                tan así que la diferencia de temperatura en una calle con arbolado a otra que no posee puede variar de 2°C a 4°C 
+            <br><br>
+                Mejoran la calidad del aire contribuyendo a su purificación. Esto se debe a que su
+                vegetación actúa filtrando los vientos, las partes aéreas de sus plantas son capaces
+                de fijar las partículas de polvo y los gases contaminantes que se encuentran suspendidos en la atmósfera, reteniendo polvos, cenizas, humos y demás impurezas que
+                son arrastradas por los vientos.
+                <p class="parrafoArt">  La mayoría de las ciudades que son densamente pobladas alcanzan niveles de ruido
+                que son pocos saludables. Los árboles y la vegetación pueden ayudar a disminuir la
+                contaminación por ruidos de distintas maneras, por la absorción del sonido, por su
+                desviación alterando su dirección, por reflexión haciendo que el sonido rebote a su
+                fuente de origen, por refracción ya que las ondas de sonidos se doblan alrededor de
+                14 un objeto, y por ocultación cuando se cubre el sonido no deseado con otro más placentero, por ejemplo, con el sonido de la naturaleza (el canto de las aves, el crispar
+                de las hojas, etc.)
+                <br><br>
+                Ayudan a solucionar paulatinamente la incorrecta disposición de los desechos sólidos urbanos. En la actualidad se ha vuelto más común visualizar que los gobiernos
+                locales, que no encuentran solución a la mala disposición de los residuos por parte
+                de sus habitantes, intentan ponerle fin y sanear lugares donde se producían microbasurales con la creación de espacios verdes con zonas de juegos para los niños. De
+                manera similar ocurre la creación de parques sobres los rellenos sanitarios ya terminados. Al plantar vegetación sobre estos y otros sitios restaurados, una ciudad puede
+                suplantar lugares que eran considerados antiestéticos por otros que resultan ser más
+                atractivos para la sociedad.
+                <br><br>
+                Mejoran la estética de las ciudades, suficientes espacios verdes hacen que determinadas zonas de una ciudad sean estéticamente más placenteras resultando más atractivas tanto para los residentes, como turistas y hasta inversionistas. Por lo tanto, la
+                presencia de estos espacios en muchos casos hace que aumenten el valor de las propiedades cercanas.
+                </p>
+            </div>
+            <br><br>
+            <hr>
+            <iframe src="" frameborder="0"></iframe>
+        </section>
+
+        <form class="formu" action="" method="post">
+            <h4>Registro de EVPU</h4>
+            <label for="nombre"> Nombre: </label>
+            <input class="formuEntrada" type="text" name="nombre" id="nombre">
+            <label for="edad"> Edad: </label>
+            <input class="formuEntrada" type="text" name="Edad" id="edad">
+            <label for="ciudad"> Localidad </label>
+            <input class="formuEntrada" type="text" name="ciudad" id="ciudad">
+            <label for="conocer"> ¿Conoces algun EVPU? </label>
+            <br><p>Si</p>
+            <input type="radio" name="preg1" value="opcion1"/>
+            <br><p>No</p> 
+            <input type="radio" name="preg2" value="opcion2"/>
+            <br>
+            <label for="frecuencia"> ¿Con que frecuencia lo visitas? </label>
+            <input class="formuEntrada" type="text" name="frecuencia" id="frecuencia">
+            <label for="area"> ¿Que te gustaria cambiar del mismo? </label>
+            <textarea class="formuEntrada" name="area" id="area"cols="30" rows="20"> </textarea>
+        <div>
+            <input class="formubotton" type="submit" value="Enviar"/>
+            <input class="formubotton" type="reset" value="Borrar campos">   
+        </div>
+        </form>
+
+    </main>
+    <br><br><br>
+    <footer> Camila Rivero - Estudiante de Gestion Ambiental </footer>
+</body>
+</html>
